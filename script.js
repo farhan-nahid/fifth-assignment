9afd9d2f2a6435ef27bad554f58ff34c281f0135
farhan-nahid <nahidahmed9933@gmail.com>
2021-02-08 15:05:39 +0600

    not perfect

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const searchFood  =     () =>{
   const searchText = document.getElementById("search-field").value;
   const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
-  // load data
+
+  // load data form api
 fetch (url)
 .then(res => res.json())
 .then (data => displayFoods(data.meals)) 
@@ -11,7 +12,7 @@ const displayFoods = foods => {
   console.log(foods);
   const foodContainer = document.getElementById("food-container");
 
-  if (searchText <= 0 ){
+  if (searchText === null){
     foodContainer.innerHTML = `
        <div>
          <h1> Sorry !!  ${searchText} This food is not Available  our Restaurant. Please Order Another Food.  </h1>
@@ -26,7 +27,7 @@ else{
     const foodDiv = document.createElement("div");
     foodDiv.className.add = ' food-card-area ';
     foodDiv.innerHTML=`
-                  <div   onclick="getDetails('${food.idMeal}')" class="card">
+                  <div   onclick="getDetails('${food.idMeal}')" class="card shadow rounded" >
                     <img src="${food.strMealThumb}" class="card-img-top" alt="..." />
                     <div class="card-body">
                       <h5 class="text-center">${food.strMeal}</h5>
@@ -50,14 +51,20 @@ const getDetails = mealId => {
 const displayMealsDetails = details => {
     const mealsDetails = document.getElementById('food-details-area');
     mealsDetails.innerHTML = `
-        <div class="d-flex justify-content-center mb-5">
-            <div class="card w-75 border-0 rounded-3 meal-details">
+        <div class="modal-dialog  mb-5 bg-light">
+            <div class="class="modal-content shadow rounded">
+            <div class="modal-header border-0">
+            <h5 class="modal-title">Food Details</h5>
+          </div>
+                <div class="modal-body p-4">
                 <img src="${details.strMealThumb}" class="card-img-top">
-                <div class="card-body bg-light">
+                <div>
                     <h1 class="mb-4">${details.strMeal}</h1>
                     <h5 class="card-text mb-4">Ingredients</h5>
                     <div id="ingredients">
                    </div>
+                   <p class=" mt-4 md-0 footer" >  &copy; by Fahan Ahmed Nahid. </p>
+                </div>
                 </div>
             </div>
         </div>
